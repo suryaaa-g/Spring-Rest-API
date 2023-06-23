9cbcb40e669104b7c010334a91dcd481567c8207
suryaaa-g <>
2023-06-23 10:45:09 -0400

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into TODO (id, description, done, target_date, username)
+values (10001, 'Learn AWS', false, CURRENT_DATE(), 'in28minutes');
+
+insert into TODO (id, description, done, target_date, username)
+values (10002, 'Learn Azure', false, CURRENT_DATE(), 'in28minutes');
+
+insert into TODO (id, description, done, target_date, username)
+values (10003, 'Learn Google Cloud', false, CURRENT_DATE(), 'in28minutes');
